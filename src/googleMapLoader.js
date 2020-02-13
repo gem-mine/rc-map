1356e7259b85fc6c6200eda784ff59d389015a15
mraiguo <810158465@qq.com>
2020-02-13 21:16:36 +0800

    //maps.google.cn 失效 修改为 //maps.google.com

--- src/googleMapLoader.js
+++ src/googleMapLoader.js
@@ -1,6 +1,6 @@
 import scriptjsLoader from '@sdp.nd/js-async-loader'
 export default function googleMapLoader (sdkUrlParams) {
   sdkUrlParams.region = sdkUrlParams.region || 'CN'
-  const url = sdkUrlParams.region.toLowerCase() === 'cn' ? '//maps.google.cn' : '//maps.googleapis.com'
+  const url = sdkUrlParams.region.toLowerCase() === 'cn' ? '//maps.google.com' : '//maps.googleapis.com'
   return scriptjsLoader(url + '/maps/api/js', 'google.maps', sdkUrlParams, 'callback')
 }
