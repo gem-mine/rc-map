5258f42fbd47c55be725e756553d6c253686d825
mraiguo <810158465@qq.com>
2020-02-13 21:41:19 +0800

    maps.google.cn 失效改为 maps.google.com

--- src/googleMapLoader.js
+++ src/googleMapLoader.js
@@ -1,6 +1,6 @@
 import scriptjsLoader from '@gem-mine/js-async-loader'
 export default function googleMapLoader (sdkUrlParams) {
   sdkUrlParams.region = sdkUrlParams.region || 'CN'
-  const url = sdkUrlParams.region.toLowerCase() === 'cn' ? '//maps.google.cn' : '//maps.googleapis.com'
+  const url = sdkUrlParams.region.toLowerCase() === 'cn' ? '//maps.google.com' : '//maps.googleapis.com'
   return scriptjsLoader(url + '/maps/api/js', 'google.maps', sdkUrlParams, 'callback')
 }
